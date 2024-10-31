52e797055b2a1c258319f616f846515d3a32b344
Nick - Ngoc Pham <tuanngocptn@users.noreply.github.com>
2024-10-31 11:23:49 +0700

    chore: noversion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import undetected_chromedriver as uc
 
+uc.TARGET_VERSION = 130
 driver = uc.Chrome()
 driver.get(
     "https://www.khmertimeskh.com/501583112/cambodia-urges-cooperation-between-local-and-international-researchers/"
